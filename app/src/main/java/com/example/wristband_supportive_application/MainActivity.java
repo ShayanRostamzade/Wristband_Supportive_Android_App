7e6585d35273fa64e1c111b0bbbbda129a3f9b61
Shayan <shayanrostamzade@Shayans-MacBook-Pro.local>
2021-08-13 15:58:21 +0430

    Second commit

--- app/src/main/java/com/example/wristband_supportive_application/MainActivity.java
+++ app/src/main/java/com/example/wristband_supportive_application/MainActivity.java
@@ -10,24 +10,40 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
 
+import java.nio.ByteBuffer;
+
 public class MainActivity extends AppCompatActivity {
+
+    //todo: change the UI
+    //todo: stop plotting when the finish button is pressed
+    //todo: stop the plotting when the danger zone has been entered
+    private static final String TAG = "MyApp";
 
     Button btnListen, btnDevice, btnStart, btnFinish;
     TextView tvStatus, tvData;
@@ -53,6 +69,26 @@ public class MainActivity extends AppCompatActivity {
     private static final String APP_NAME = "Wristband_Supportive_Application";
     private static final UUID MY_UUID=UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    /*
+    Graph property section
+     */
+    // the number of points plotted on screen considering 5 data points below
+    private int pointsPlotted = 5;
+
+    private Viewport viewport;
+
+    // the amount of time between showing the
+    private int graphIntervalCounter = 0;
+
+    // the y coordinate is the HR or SPO2 values
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+            new DataPoint(0, 1),
+            new DataPoint(1, 5),
+            new DataPoint(2, 3),
+            new DataPoint(3, 2),
+            new DataPoint(4, 6)
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +108,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         implementListeners();
+
+        // Sample graph code
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        viewport = graph.getViewport();
+        viewport.setScalable(true);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setYAxisBoundsManual(true);
+        graph.addSeries(series);
+
     }
 
     private void findViewByID(){
@@ -110,19 +155,16 @@ public class MainActivity extends AppCompatActivity {
         btnListen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ServerClass serverClass=new ServerClass();
+                ServerClass serverClass = new ServerClass();
                 serverClass.start();
             }
         });
 
-        lvPairedDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ClientClass clientClass = new ClientClass(btArray[i]);
-                clientClass.start();
+        lvPairedDevices.setOnItemClickListener((adapterView, view, i, l) -> {
+            ClientClass clientClass = new ClientClass(btArray[i]);
+            clientClass.start();
 
-                tvStatus.setText("Connecting");
-            }
+            tvStatus.setText("Connecting");
         });
 
         btnStart.setOnClickListener(new View.OnClickListener() {
@@ -147,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // disconnect the bluetooth and close the socket
-
+                connectionFlag = false;
             }
         });
     }
@@ -171,9 +213,63 @@ public class MainActivity extends AppCompatActivity {
                     tvStatus.setText("Connection Failed");
                     break;
                 case STATE_MESSAGE_RECEIVED:
-                    byte[] readBuff= (byte[]) msg.obj;
-                    String tempMsg=new String(readBuff,0,msg.arg1);
-                    tvData.setText(tempMsg);
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff, 0, msg.arg1);
+                    // todo: if data is more than 45 then ...
+                    char[] temp = new char[tempMsg.length()];
+                    char first = 0;
+                    char third = 0;
+                    char fourth = 0;
+                    for (int i = 0; i < tempMsg.length(); i++) {
+                        temp[i] = tempMsg.charAt(i);
+                        if(i == 0)
+                            first = temp[i];
+                        if(i == 2)
+                            third = temp[i];
+                        if(i == 3)
+                            fourth = temp[i];
+                    }
+                    if(first == third){
+                        char[] trimmed2 = new char[2];
+                        for(int i = 0; i < 2; i++){
+                            trimmed2[i] = temp[i];
+                        }
+                        tempMsg = String.valueOf(trimmed2);
+                    }
+                    else if(first == fourth){
+                        char[] trimmed3 = new char[3];
+                        tempMsg = String.valueOf(trimmed3);
+                        for(int i = 0; i < 3; i++){
+                            trimmed3[i] = temp[i];
+                        }
+                    }
+
+
+//                    String _msgInt = (String) msg.obj;
+//                    String tempMsg = String.valueOf(_msgInt);
+                    try{
+                        int _msgInt = Integer.parseInt(tempMsg);
+//                        int _msgInt = ByteBuffer.wrap(readBuff).getShort();
+                        if(countDigit(_msgInt) >= 2){
+                            tvData.setText(tempMsg);
+
+                            //update the graph
+                            pointsPlotted++;
+                            // resetting the chart if data points are exceeding 5000 samples
+                            if(pointsPlotted > 5000){
+                                pointsPlotted = 1;
+                                series.resetData(new DataPoint[] {new DataPoint(1, 0)});
+                            }
+                            series.appendData(new DataPoint(pointsPlotted, _msgInt), true, pointsPlotted);
+                            viewport.setMaxX(pointsPlotted);
+                            viewport.setMaxY(200);
+                            // removing the last 200 points from the chart
+                            viewport.setMinX(pointsPlotted - 100);
+                            Log.e(TAG, "m_data: " + _msgInt);
+                        }
+                    }catch (NumberFormatException e){
+                        e.printStackTrace();
+                    }
                     break;
             }
             return true;
@@ -199,11 +295,11 @@ public class MainActivity extends AppCompatActivity {
             while (socket == null)
             {
                 try {
-                    Message message=Message.obtain();
-                    message.what=STATE_CONNECTING;
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTING;
                     handler.sendMessage(message);
 
-                    socket=serverSocket.accept();
+                    socket = serverSocket.accept();
                 } catch (IOException e) {
                     e.printStackTrace();
                     Message message = Message.obtain();
@@ -287,15 +383,19 @@ public class MainActivity extends AppCompatActivity {
 
         public void run()
         {
-            byte[] buffer = new byte[1024];
-            int bytes;
+            byte[] buffer = new byte[16];
+            int bytes = 0;
+//            DataInputStream dataInputStream = new DataInputStream(inputStream);
+//            String data;
+//            int dataLength;
 
-            while (true)
+            while (bytes != -1)
             {
                 try {
                     if(connectionFlag){
-                        bytes = inputStream.read(buffer);
-                        handler.obtainMessage(STATE_MESSAGE_RECEIVED,bytes,-1,buffer).sendToTarget();
+//                        data = dataInputStream.readUTF();
+                        bytes = inputStream.read(buffer, 0, 6);
+                        handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -312,4 +412,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    public int countDigit(int n)
+    {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            ++count;
+        }
+        return count;
+    }
+
+//    public char[] pattern(String in){
+//        int pattern_end = 0;
+//        for(int i = pattern_end+1 ; i < in.length(); i++) {
+//            int pattern_dex = i % (pattern_end + 1);
+//            if (in[pattern_dex] != in[i]) {
+//                pattern_end = i;
+//                continue;
+//            }
+//            if (i == in.length - 1) {
+//                return in[0:pattern_end + 1];
+//
+//            }
+//        }
+//        return in;
+//    }
+
 }
